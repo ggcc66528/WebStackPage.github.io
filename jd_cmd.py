68c58c7c5766605f0cda5fc60ceb2be62bde8f74
cc66528 <41284877+cc66528@users.noreply.github.com>
2022-02-23 14:02:42 +0800

    Update jd_cmd.py

--- jd_cmd.py
+++ jd_cmd.py
@@ -23,6 +23,9 @@ async def jd_cmd(context):
         data = data["data"]
         await context.edit(f"{data['jumpUrl']}")
         ##await context.edit(f"\"{Url}\"")
-        await context.edit(f"{jumpUrl['activityId=']}")
+        await context.edit(f"{data['jumpUrl']}-{data['title']}")
+   ## try:
+   ##     data = data["data"]
+   ##     await context.edit(f"[jd_cmd] [【{data['title']}】 - {data['userName']}]({data['jumpUrl']})")        
     except KeyError:
         return await context.edit("[jd_cmd] 数据错误！")
